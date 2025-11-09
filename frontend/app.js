8dd09c24e35369230085339f8cd2e96bc17874f0
hemanthkumarrevalla-presents <hemanthkumarrevalla@gmail.com>
2025-11-10 00:51:17 +0530

    backend deployed

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = window.API_BASE_URL || "http://localhost:4000";
+const API_BASE = window.API_BASE_URL || "https://project-1-rosy-one.vercel.app/"
 const MODE_LABELS = {
   normal: "Normal Flow",
   rush_hour: "Rush Hour Surge",
